bf7331a55399aaeb923427fda36f31e338a17a9a
T <80172200+taiscoding@users.noreply.github.com>
2025-09-24 04:15:38 -0400

    Initial Commit

---
+++ medschoolrpg/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  medschoolrpg
+//
+//  Created by Theodore Addo on 9/24/25.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
